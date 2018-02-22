9af0e807d5d5d2ff6dbbc2cd1b59eba1341204ab
Isidore <isidorebaldado@knights.ucf.edu>
2018-02-22 10:37:25 -0700

    Setup a 8x8 grid. Grid will also read an 8x8 array and show it

--- Checkers/Checkers/CheckersViewController.swift
+++ Checkers/Checkers/CheckersViewController.swift
@@ -11,12 +11,28 @@ import UIKit
 class CheckersViewController: UIViewController {
     
     // Mock Model Controller
-    var gameState: [[Int]]?
+    var gameState: [[Int]] =
+    [[1,1,1,1,1,1,1,1],
+     [1,1,1,1,1,1,1,1],
+     [0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0],
+     [0,0,0,0,0,0,0,0],
+     [2,2,2,2,2,2,2,2],
+     [2,2,2,2,2,2,2,2]]
+    
+    
+    // Black Magic for setting up size
+    fileprivate let sectionInsets = UIEdgeInsets(top: 2.5, left: 0.0, bottom: 2.5, right: 0.0)
+    fileprivate let itemsPerRow: CGFloat = 9
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("DEBUG: Row Count: \(gameState.count)")
+        print("DEBUG: Column Count: \(gameState[0].count)")
 
         // Do any additional setup after loading the view.
     }
@@ -50,11 +66,47 @@ extension CheckersViewController: UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let colors = [UIColor.green, UIColor.blue, UIColor.red]
+        
+        let debugState = gameState[indexPath.section][indexPath.row]
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "checkersCell", for: indexPath)
+        cell.backgroundColor = colors[debugState]
+        
+        return cell
     }
     
 }
 
 extension CheckersViewController: UICollectionViewDelegate {
+ 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected: Sect:\(indexPath.section) Row:\(indexPath.row)")
+    }
+}
+
+extension CheckersViewController: UICollectionViewDelegateFlowLayout {
     
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 }
