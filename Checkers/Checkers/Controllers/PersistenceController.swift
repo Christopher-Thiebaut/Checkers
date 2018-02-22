c9b582ee501ede5656546161db91f5cff132c7dc
Christopher Thiebaut <christopherthiebaut@gmail.com>
2018-02-22 15:00:50 -0700

    Organize project into folders.  Add empty PersistenceController that will later be used to save game state.

---
+++ Checkers/Checkers/Controllers/PersistenceController.swift
@@ -0,0 +1,9 @@
+//
+//  PersistenceController.swift
+//  Checkers
+//
+//  Created by Christopher Thiebaut on 2/22/18.
+//  Copyright © 2018 Christopher Thiebaut. All rights reserved.
+//
+
+import Foundation
